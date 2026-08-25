be0b58fb172e3a176ae6017ca574622fe943c532
Aditya Kumar Rouniyar <adityarouniyar99@gmail.com>
2026-08-25 18:31:49 +0530

    Time: 12 ms (37.60%) | Memory: 52 MB (99.31%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,7 +1,7 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        Map<Integer,Integer> mpp = new TreeMap<>();
+        Map<Integer,Integer> mpp = new HashMap<>();
 
         for(int i=0;i<n;i++){
             mpp.put(nums[i], mpp.getOrDefault(nums[i],0) + 1);
