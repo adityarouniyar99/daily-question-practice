cb27f1d1a31577f63861f50782a828ae76a54315
Aditya Kumar Rouniyar <adityarouniyar99@gmail.com>
2026-08-27 18:45:57 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (23.33%) - LeetSync

--- 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
+++ 2144-maximum-difference-between-increasing-elements/maximum-difference-between-increasing-elements.java
@@ -5,9 +5,11 @@ class Solution {
         int mini = nums[0];
         int profit = 0;
         for(int i=1;i<n;i++){
-            int cost = nums[i] - mini;
-            profit = Math.max(profit,cost);
-            mini = Math.min(mini,nums[i]);
+            if(nums[i]>mini){
+                profit = Math.max(profit,nums[i]-mini);
+            }else{
+                mini = nums[i];
+            }
         }
         if(profit>0){
             return profit;
