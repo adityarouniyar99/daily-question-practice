2d2c5e8a5f3d30f95c51c8bb32b73e3211eb43d8
Aditya Kumar Rouniyar <adityarouniyar99@gmail.com>
2026-09-16 23:33:53 +0530

    Time: 1 ms (96.00%) | Memory: 47.7 MB (42.00%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -2,62 +2,61 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m = matrix.length;
         int n = matrix[0].length;
+    //==============================================================
+    // Better soln
+    // boolean[] row = new boolean[m];
+    // boolean[] col = new boolean[n];
 
-    //     if (matrix==null ||matrix.length <= 1 || matrix[0].length <= 1 ) {
-    //         return;
-    //     }
-
-    //     for (int i = 0; i < m; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if (matrix[i][j] == 0) {
-    //                 markRow(i, matrix, n);
-    //                 markCol(j, matrix, m);
-    //             }
-    //         }
-    //     }
-
-    //     for (int i = 0; i < m; i++) {
-    //         for (int j = 0; j < n; j++) {
-    //             if (matrix[i][j] == -1) {
-    //                 matrix[i][j] = 0;
-    //             }
+    // for(int i=0;i<m;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(matrix[i][j]==0){
+    //             row[i] = true;
+    //             col[j] = true;
     //         }
     //     }
     // }
 
-    // public void markRow(int i, int[][] matrix, int n) {
-    //     for (int j = 0; j < n; j++) {
-    //         if (matrix[i][j] != 0) {
-    //             matrix[i][j] = -1;
+    // for(int i=0;i<m;i++){
+    //     for(int j=0;j<n;j++){
+    //         if(row[i] || col[j] ){
+    //             matrix[i][j] = 0;
     //         }
     //     }
     // }
 
-    // public void markCol(int j, int[][] matrix, int m) {
-    //     for (int i = 0; i < m; i++) {
-    //         if (matrix[i][j] != 0) {
-    //             matrix[i][j] = -1;
-    //         }
-    //     }
-    // }
-
-    boolean[] row = new boolean[m];
-    boolean[] col = new boolean[n];
-
+    //==============================================================
+    //optimal
+    int col0 = 1;
     for(int i=0;i<m;i++){
         for(int j=0;j<n;j++){
             if(matrix[i][j]==0){
-                row[i] = true;
-                col[j] = true;
+                matrix[i][0]=0;
+                if(j!=0){
+                    matrix[0][j] = 0;
+                }else{
+                    col0 = 0;
+                }
+            }
+        }
+    }
+    
+    for(int i=1;i<m;i++){
+        for(int j=1;j<n;j++){
+            if(matrix[i][j]!=0){
+                if(matrix[0][j]==0 || matrix[i][0]==0){
+                    matrix[i][j]=0;
+                }
             }
         }
     }
 
-    for(int i=0;i<m;i++){
-        for(int j=0;j<n;j++){
-            if(row[i] || col[j] ){
-                matrix[i][j] = 0;
-            }
+    if(matrix[0][0]==0){
+        for(int j=0;j<n;j++) matrix[0][j]=0;
+    }
+
+    if(col0==0){
+        for(int i=0;i<m;i++){
+            matrix[i][0]=0;
         }
     }
 
