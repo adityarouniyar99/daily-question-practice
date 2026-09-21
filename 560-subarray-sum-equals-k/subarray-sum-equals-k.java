bd9739dd273b7996e812f439d7fbffa16bbc5e9b
Aditya Kumar Rouniyar <adityarouniyar99@gmail.com>
2026-09-21 18:46:26 +0530

    Time: 1536 ms (19.65%) | Memory: 48.3 MB (96.38%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,22 +1,34 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count = 0;
+
+//BRUTE FORCE
+        // int count = 0;
+        // int n = nums.length;
+        // for(int i=0;i<n;i++){
+        //     for(int j=i;j<n;j++){
+        //         int sum = 0;
+        //         for(int l=i;l<=j;l++){
+        //             sum += nums[l];
+        //         }
+        //         if(sum==k){
+        //             count++;
+        //         }
+        //     }
+        // }
+
+        // return count;
+
+// BETTER
         int n = nums.length;
+        int count = 0;
+
         for(int i=0;i<n;i++){
             int sum = 0;
             for(int j=i;j<n;j++){
-                // int sum = 0;
-                // for(int l=i;l<j;l++){
-                //     sum += nums[l];
-                // }
                 sum += nums[j];
                 if(sum==k){
                     count++;
                 }
             }
-        }
-
-        return count;
-
-    }
-}
+        }return count;
+}}
