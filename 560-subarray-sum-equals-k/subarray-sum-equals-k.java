74bdd2c1de2ef6b920e1d9e56b8ccf20d06cfb5b
Aditya Kumar Rouniyar <adityarouniyar99@gmail.com>
2026-09-21 19:35:51 +0530

    Time: 24 ms (75.44%) | Memory: 49.1 MB (22.78%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -19,16 +19,36 @@ class Solution {
         // return count;
 
 // BETTER
-        int n = nums.length;
-        int count = 0;
+        // int n = nums.length;
+        // int count = 0;
 
+        // for (int i = 0; i < n; i++) {
+        //     int sum = 0;
+        //     for (int j = i; j < n; j++) {
+        //         sum += nums[j];
+        //         if (sum == k) {
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
+
+// OPTIMAL
+        int n = nums.length;
+        HashMap<Integer,Integer> mpp = new HashMap<>();
+        mpp.put(0,1);
+        int preSum = 0;
+        int count = 0;
         for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                sum += nums[j];
-                if(sum==k){
-                    count++;
-                }
+            preSum += nums[i];
+            int remove = preSum - k;
+            
+            if (mpp.containsKey(remove)) {
+                count += mpp.get(remove);
             }
-        }return count;
-}}
+
+            mpp.put(preSum,mpp.getOrDefault(preSum,0)+1);            
+        }
+        return count;
+    }
+}
