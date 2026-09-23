154f9566875035a630b5d2a95702ccb4f7c5a3af
Aditya Kumar Rouniyar <adityarouniyar99@gmail.com>
2026-09-23 22:29:23 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (7.48%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,20 +1,36 @@
 class Solution {
-    public long funNCR(int n, int r) {
-        long res = 1;
 
-        for (int i = 1; i <=r; i++) {
-            res = res * (n - i + 1) / i;
-        }
+// =====================BRUTE FORCE =================================
+    // public long funNCR(int n, int r) {
+    //     // here  n is row and r is column
+    //     long res = 1;
 
-        return res;
-    }
+    //     for (int i = 1; i <=r; i++) {
+    //         res = res * (n - i + 1) / i;
+    //     }
 
+    //     return res;
+    // }
+//====================================================================
     public List<Integer> getRow(int rowIndex) {
         List<Integer> result = new ArrayList<>();
 
-        for (int c = 0; c <= rowIndex; c++) {
-            result.add((int)funNCR(rowIndex,c));
+// BRUTE FORCE SOLUTION
+        // for (int c = 0; c <= rowIndex; c++) {
+        //     result.add((int)funNCR(rowIndex,c));
+        // }
+        // return result;
+
+
+// =======================BETTER============================================
+
+        long current = 1;
+        result.add((int)current);
+        for(int i=1;i<=rowIndex;i++){
+            current = current * (rowIndex-i+1)/i;
+            result.add((int)current);
         }
+
         return result;
     }
 }
